a863a6663fae6a9767770dda45c46915b4e41147
Max Miao <miaoma2@oocl.com>
2020-08-27 17:37:51 +0800

    [refactoring][max] refactor: refactor method statement

--- src/statement.js
+++ src/statement.js
@@ -35,20 +35,32 @@ function formatToUSD() {
 }
 
 function statement (invoice, plays) {
+  let items = {};
+  items["customer"] = invoice.customer;
+  items["data"] = [];
   let totalAmount = 0;
   let volumeCredits = 0;
-  let result = `Statement for ${invoice.customer}\n`;
+  let result = `Statement for ${items["customer"]}\n`;
   const format = formatToUSD();
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
     let thisAmount = calThisAmount(perf, play);
     volumeCredits = calVolumeCredits(volumeCredits, perf, play);
     //print line for this order
+    items["data"].push(
+        {
+          playName: play.name,
+          amount: format(thisAmount / 100),
+          audience: perf.audience
+        }
+    );
     result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
     totalAmount += thisAmount;
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
-  result += `You earned ${volumeCredits} credits \n`;
+  items["totalAmount"] = format(totalAmount / 100);
+  items["volumeCredits"] = volumeCredits;
+  result += `Amount owed is ${items["totalAmount"]}\n`;
+  result += `You earned ${items["volumeCredits"]} credits \n`;
   return result;
 }
 
@@ -67,3 +79,30 @@ module.exports = {
 //     '</table>\n' +
 //     '<p>Amount owed is <em>$1,730.00</em></p>\n' +
 //     '<p>You earned <em>47</em> credits</p>\n');
+
+function createStatementDate(invoice, plays){
+  let items = {};
+  items["customer"] = invoice.customer;
+  items["data"] = [];
+  console.log(items)
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  const format = formatToUSD();
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+    let thisAmount = calThisAmount(perf, play);
+    volumeCredits = calVolumeCredits(volumeCredits, perf, play);
+    items["data"].push(
+        {
+          playName: play.name,
+          amount: format(thisAmount / 100),
+          audience: perf.audience
+        }
+    );
+    totalAmount += thisAmount;
+  }
+  items["totalAmount"] = totalAmount;
+  items["volumeCredits"] = volumeCredits;
+  console.log(items)
+  return items
+}
